a4a86579fd59237798b0aad532b9486ac06b8cbd
mohammedigbal <mohammedigbal@yahoo.com>
2024-08-31 19:17:05 +0530

    Updated db related configurations

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    user: "admin",
-    password: "admin",
-    host: "localhost",
+    user: "postgres",
+    password: "crmadminuser",
+    //host: "localhost",
+    host: "postgres://postgres:crmadminuser@crm-db-instance.chyi08eiss0b.us-east-1.rds.amazonaws.com/postgres"
     port: 5432,
-    database: "crm"
+    database: "postgres"
 })
 
 module.exports = pool;
