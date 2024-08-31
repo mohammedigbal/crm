944285428a50131bcb25272e05d01a700b929ff3
mohammedigbal <mohammedigbal@yahoo.com>
2024-08-31 22:43:52 +0530

    db connectionstring update

--- server/database.js
+++ server/database.js
@@ -4,9 +4,9 @@ const pool = new Pool({
     user: "postgres",
     password: "crmadminuser",
     //host: "localhost",
-    host: "postgres://postgres:crmadminuser@crm-db-instance.chyi08eiss0b.us-east-1.rds.amazonaws.com/postgres"
+    host: "crm-db-instance.chyi08eiss0b.us-east-1.rds.amazonaws.com"
     port: 5432,
-    database: "postgres"
+    database: "crm_db"
 })
 
 module.exports = pool;
