caaba2d172b23e902b99e7b5d7bdbcbf4b20c956
mohammedigbal <mohammedigbal@yahoo.com>
2024-09-04 19:13:29 +0530

    Updated host

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     password: "crmadminuser",
     //host: "localhost",
-    host: "crm-db-instance.chyi08eiss0b.us-east-1.rds.amazonaws.com",
+    host: "crm-db-instance.cncooa6u6jo0.ap-south-1.rds.amazonaws.com",
     port: 5432,
     database: "crm_db"
 })
