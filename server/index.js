afc1f33a57d8f4d06a16007e987f5595664194eb
mohammedigbal <mohammedigbal@yahoo.com>
2024-09-05 21:37:35 +0530

    updated rds endpoint

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(cors({ origin: url }));
 const pool = new Pool({
     user: "postgres",
     password: "crmadminuser",
-    host: "localhost",
-    //host: "crm-db-instance.cncooa6u6jo0.ap-south-1.rds.amazonaws.com",
+    //host: "localhost",
+    host: "crm-db-instance.cncooa6u6jo0.ap-south-1.rds.amazonaws.com",
     port: 5432,
     database: "crm_db"
 })
